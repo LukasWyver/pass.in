d9d9787c344766a466002428a7058a866e29765f
lukaswyver <lucas_sin5@hotmail.com>
2024-04-02 23:24:07 -0300

    feat: created event table

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,9 +1,31 @@
 import fastify from 'fastify'
+import { z } from 'zod'
+import { db } from './lib/prisma'
+import { generateSlug } from './utils/generate-slug'
 
 const app = fastify()
 
-app.get('/', () => {
-  return 'Hello nlw unite'
+app.post('/events', async (request, reply) => {
+  const createEventSchema = z.object({
+    title: z.string().min(4),
+    details: z.string().nullable(),
+    maximumAttendees: z.number().int().positive().nullable()
+  })
+
+  const data = createEventSchema.parse(request.body)
+
+  const { title, details, maximumAttendees} = data
+
+  const event = await db.event.create({
+    data: {
+      title,
+      details,
+      maximumAttendees,
+      slug: generateSlug(title)
+    }
+  })
+
+  return reply.status(201).send({ eventId: event.id })
 })
 
 app.listen({ port: 3333 }).then(() => {
